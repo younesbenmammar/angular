2e33e01a484113d3e051428a445bc0b82cdcf7fc
younesbenmammar <younescomptepro@gmail.com>
2018-03-28 16:21:27 +0200

    1

---
+++ src/app/hero.ts
@@ -0,0 +1,18 @@
+export class Hero {
+
+        id : number;
+
+
+        name : string;
+
+
+        constructor(id : number , name : string){
+
+            this.id = id;
+            this.name = name;
+
+        }
+
+
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero'; // importe la classe Hero
+
+export const HEROES: Hero[] = [  // exporte la constante HEROES qui est un tableau
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Flash' },
+  { id: 13, name: 'Superman' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'Hulk' },
+
+];
